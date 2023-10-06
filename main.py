3f08ce41c5b2db7876be16bd86af7e1bbb87085d
Venkatesh <shotovenkatesh705@gmail.com>
2023-10-06 15:19:22 +0400

    Name fix

--- main.py
+++ main.py
@@ -40,37 +40,66 @@ last_element = len(name_from_csv)
 while index < last_element:
     food = name_from_csv[index]
 
+
     food_c = category_from_csv[index]
 
     driver.find_element(By.XPATH,
                         '/html/body/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div[2]/div/label/span/input').send_keys(
         food)
     time.sleep(3)
+    table = driver.find_element(By.ID,"DataTables_Table_0")
+    rows = table.find_elements(By.TAG_NAME,"tr")
+    first_td = rows[1].text
+    try:
+        second_td = rows[2].text
 
-    odd_row = driver.find_element(By.CLASS_NAME, 'odd')
-    td_element = odd_row.find_element(By.CLASS_NAME, 'td-actions.text-right')
-    button = td_element.find_element(By.CLASS_NAME, 'btn.btn-success')
-    button.click()
-    time.sleep(2)
-    driver.execute_script('scrollBy(0,50)')
 
-    # this is  to locate the category field
-    driver.find_element(By.XPATH,
-                        '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/button/div[1]/div/div').click()
-    time.sleep(2)
-    # this is to search in the category field
-    driver.find_element(By.XPATH,
-                        '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/div/div[2]/input').send_keys(
-        food_c)
-    time.sleep(1)
-    driver.find_element(By.XPATH,
-                        '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/div/div[3]/ul/li[1]/a').click()
-    driver.execute_script('scrollBy(0,500)')
+        if "No image" in first_td:
+            first = first_td.split("No image")[0]
+        elif "image" in first_td:
+            first = first_td.split("image")[0]
+        else:
+            first = first_td
 
-    submit = driver.find_element(By.NAME, 'submit').click()
-    print(f"{food} has been added")
-    index += 1
-    time.sleep(2)
+        if "No image" in second_td:
+            second = second_td.split("No image")[0]
+        elif "image" in second_td:
+            second = second_td.split("image")[0]
+        else:
+            second = second_td
+
+        if first == second:
+            with open('duplicate_products.csv', 'a') as fd:
+                fd.write("\n")
+                fd.write(str(first))
+
+    except:
+        print(f"no duplicates for {food}")
+    finally:
+        odd_row = driver.find_element(By.CLASS_NAME, 'odd')
+        td_element = odd_row.find_element(By.CLASS_NAME, 'td-actions.text-right')
+        button = td_element.find_element(By.CLASS_NAME, 'btn.btn-success')
+        button.click()
+        time.sleep(2)
+        driver.execute_script('scrollBy(0,50)')
+
+        # this is  to locate the category field
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/button/div[1]/div/div').click()
+        time.sleep(2)
+        # this is to search in the category field
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/div/div[2]/input').send_keys(
+            food_c)
+        time.sleep(1)
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div[2]/div[4]/div[1]/div/div/div/div[3]/ul/li[1]/a').click()
+        driver.execute_script('scrollBy(0,500)')
+
+        submit = driver.find_element(By.NAME, 'submit').click()
+        print(f"{food} has been added")
+        index += 1
+        time.sleep(2)
 
 
 
---
+++ test.py
@@ -0,0 +1,4 @@
+a = "0 فتة بالمكسرات / Fatteh Nuts "
+b = "0 فتة بالمكسرات / Fatteh Nuts "
+
+print(a == b )
